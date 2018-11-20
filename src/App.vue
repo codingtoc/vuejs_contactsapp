dba040009c50167e4742088dad448198da81451a
codingtoc <codingtoc@gmail.com>
2018-11-20 15:00:48 +0900

    라우팅을 적용한 연락처 애플리케이션 - App.vue 파일 변경

--- src/App.vue
+++ src/App.vue
@@ -2,47 +2,20 @@
   <div id="container">
     <div class="page-header">
       <h1 class="text-center">연락처 관리 애플리케이션</h1>
-      <p>(Dynamic Component + Vuex + Axios)</p>
+      <p>(Vue-router + Vuex + Axios)</p>
+      <div class="btn-group">
+        <router-link to="/home" class="btn btn-info menu">Home</router-link>
+        <router-link to="/about" class="btn btn-info menu">About</router-link>
+        <router-link to="/contacts" class="btn btn-info menu">Contacts</router-link>
+      </div>
     </div>
-    <component :is="currentView"></component>
-    <contact-list></contact-list>
-    <paginate ref="pagebuttons" :page-count="totalpage" :page-range="7" :margin-pages="3" :click-handler="pageChanged" :prev-text="'이전'" :next-text="'다음'" :container-class="'pagination'" :page-class="'page-item'"></paginate>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import ContactList from "./components/ContactList";
-import ContactForm from "./components/ContactForm";
-import UpdatePhoto from "./components/UpdatePhoto";
-import CONF from "./Config.js";
-import Constant from "./constant.js";
-import Paginate from "vuejs-paginate";
-import { mapState } from "vuex";
-
 export default {
-  name: "app",
-  components: { ContactList, ContactForm, UpdatePhoto, Paginate },
-  mounted() {
-    this.$store.dispatch(Constant.FETCH_CONTACTS);
-  },
-  computed: {
-    totalpage() {
-      var totalcount = this.contactlist.totalcount;
-      var pagesize = this.contactlist.pagesize;
-      return Math.floor((totalcount - 1) / pagesize) + 1;
-    },
-    ...mapState(["contactlist", "currentView"])
-  },
-  methods: {
-    pageChanged(page) {
-      this.$store.dispatch(Constant.FETCH_CONTACTS, { pageno: page });
-    }
-  },
-  watch: {
-    "contactlist.pageno"(page) {
-      this.$refs.pagebuttons.selected = page - 1;
-    }
-  }
+  name: "app"
 };
 </script>
 
@@ -56,5 +29,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.menu {
+  width: 100px;
 }
 </style>
