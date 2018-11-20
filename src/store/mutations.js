1ad5e6a117ee02c3371e2360404ac231e63132d7
codingtoc <codingtoc@gmail.com>
2018-11-20 13:44:42 +0900

    라우팅을 적용한 연락처 애플리케이션 - 변이 변경

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,24 +1,13 @@
 import Constant from "../constant";
 
 export default {
-  [Constant.ADD_CONTACT_FORM]: state => {
-    state.contact = { no: 0, name: "", tel: "", address: "", photo: "" };
-    state.mode = "add";
-    state.currentView = "contactForm";
-  },
-  [Constant.CANCEL_FORM]: state => {
-    state.currentView = null;
-  },
-  [Constant.EDIT_CONTACT_FORM]: (state, payload) => {
-    state.contact = payload.contact;
-    state.mode = "update";
-    state.currentView = "contactForm";
-  },
-  [Constant.EDIT_PHOTO_FORM]: (state, payload) => {
-    state.contact = payload.contact;
-    state.currentView = "updatePhoto";
-  },
   [Constant.FETCH_CONTACTS]: (state, payload) => {
     state.contactlist = payload.contactlist;
+  },
+  [Constant.FETCH_CONTACT_ONE]: (state, payload) => {
+    state.contact = payload.contact;
+  },
+  [Constant.INITIALIZE_CONTACT_ONE]: state => {
+    state.contact = { no: 0, name: "", tel: "", address: "", photo: "" };
   }
 };
