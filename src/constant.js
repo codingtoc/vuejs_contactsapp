0964c132f4a53689f7d253e109cd23915197fa23
codingtoc <codingtoc@gmail.com>
2018-11-21 19:21:38 +0900

    지연 시간 처리를 적용한 연락처 애플리케이션 - 상수 변경(CHANGE_ISLOADING 추가)

--- src/constant.js
+++ src/constant.js
@@ -5,5 +5,6 @@ export default {
   UPDATE_PHOTO: "updatePhoto", // 사진 수정
   DELETE_CONTACT: "deleteContact", // 연락처 삭제
   FETCH_CONTACT_ONE: "fetchContactOne", // 연락처 한건 조회
+  CHANGE_ISLOADING: "changeIsLoading",
   INITIALIZE_CONTACT_ONE: "initializeContactOne" // 추가 화면으로 전환했을 때 입력폼 초기화
 };
